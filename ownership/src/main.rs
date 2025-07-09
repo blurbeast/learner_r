6623a57179853eabad81a5b76be1dd21e0ba8edc
blurbeast <leumasre@gmail.com>
2025-07-09 20:39:38 +0100

    kitted

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("The length of s is: {}", s.len());
     println!("The capacity of s is: {}", s.capacity());
 
-    s.push_str(" from Rust");
+    s.push_str(" from Rust!!");
     println!("The value of s after push_str is: {}", s);
     println!("The length of s after push_str is: {}", s.len());
     println!("The capacity of s after push_str is: {}", s.capacity());
