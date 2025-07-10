d54f98d2c9c0db208c5da16e937cfe54f4acdc9f
blurbeast <leumasre@gmail.com>
2025-07-10 04:22:24 +0100

    struct

---
+++ struct_learner/src/main.rs
@@ -0,0 +1,22 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+struct User {
+    email: String,
+    username: String,
+    active: bool,
+    sign_in_count: u64,
+}
+
+
+// fn main() {
+
+//     let user: user = User {
+//         username: String::from("blurbeast"),
+//         active: true,
+//         sign_in_count: 0,
+//         email: String::from("email@blurbeast.co")
+//     };
+// }
