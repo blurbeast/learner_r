fc14ddb04f8c4109f2b1aa10549e64b9923520d3
blurbeast <leumasre@gmail.com>
2025-07-10 04:31:43 +0100

    still learning struct build

--- struct_learner/src/main.rs
+++ struct_learner/src/main.rs
@@ -20,3 +20,46 @@ struct User {
 //         email: String::from("email@blurbeast.co")
 //     };
 // }
+
+
+// using the dot notation to access the fields of a struct
+// and also changing the value of a field
+fn main() {
+
+    let mut user: User = User {
+        username: String::from("blurbeast"),
+        active: true,
+        sign_in_count: 0,
+        email: String::from("email@blurbeast.co"),
+    };
+
+    let new_email: String = String::from("blurrybeast@blurbeast.co");
+
+    user.email = new_email;
+}
+
+// creating a function that builds the struxt
+// because user does not have to decide the active and sign_in_count fields, we use default values
+
+fn build_user_struct(username: String, email: String) -> User {
+    // using this approach to build the struct is a really good practice
+    // User{
+    //     active: true,
+    //     sign_in_count: 0,
+    //     email: email,
+    //     username: username,
+    // }
+
+    // but what about when we have more fields?
+    // how do we cope with and how do we go about it ?
+    // this is when we parameters naming carry the sname as the struct fields
+
+    User{
+        username,
+        email,
+        active: true,
+        sign_in_count: 0,
+    }
+
+    // @Note: this is called field init shorthand
+}
