3fead30a2e194e0a274390161d789a48d17fd5aa
blurbeast <leumasre@gmail.com>
2025-07-10 10:09:07 +0100

    good will

--- struct_learner/src/main.rs
+++ struct_learner/src/main.rs
@@ -3,12 +3,12 @@
 // }
 
 
-struct User {
-    email: String,
-    username: String,
-    active: bool,
-    sign_in_count: u64,
-}
+// struct User {
+//     email: String,
+//     username: String,
+//     active: bool,
+//     sign_in_count: u64,
+// }
 
 
 // fn main() {
@@ -41,28 +41,28 @@ struct User {
 // creating a function that builds the struxt
 // because user does not have to decide the active and sign_in_count fields, we use default values
 
-fn build_user_struct(username: String, email: String) -> User {
-    // using this approach to build the struct is a really good practice
-    // User{
-    //     active: true,
-    //     sign_in_count: 0,
-    //     email: email,
-    //     username: username,
-    // }
+// fn build_user_struct(username: String, email: String) -> User {
+//     // using this approach to build the struct is a really good practice
+//     // User{
+//     //     active: true,
+//     //     sign_in_count: 0,
+//     //     email: email,
+//     //     username: username,
+//     // }
 
-    // but what about when we have more fields?
-    // how do we cope with and how do we go about it ?
-    // this is when we parameters naming carry the sname as the struct fields
+//     // but what about when we have more fields?
+//     // how do we cope with and how do we go about it ?
+//     // this is when we parameters naming carry the sname as the struct fields
 
-    User{
-        username,
-        email,
-        active: true,
-        sign_in_count: 0,
-    }
+//     User{
+//         username,
+//         email,
+//         active: true,
+//         sign_in_count: 0,
+//     }
 
-    // @Note: this is called field init shorthand
-}
+//     // @Note: this is called field init shorthand
+// }
 
 
 // fn main() {
@@ -139,19 +139,129 @@ fn build_user_struct(username: String, email: String) -> User {
 // }
 
 
-struct Rectangle(u32, u32);
+// struct Rectangle(u32, u32);
+
+// fn main() {
+
+//     // using the field init shorthand to create an instance of Rectangle
+//     let rectangle: Rectangle = Rectangle(30, 50);
+
+//     let area_of_rectangle: u32 = area(rectangle);
+
+//     println!("The area of the rectangle is: {}", area_of_rectangle);
+
+// }
+
+// fn area(rectangle: Rectangle) -> u32 {
+//     rectangle.0 * rectangle.1
+// }
+
+
+// using tuple to build common related terms
+// fn main() {
+//     let width: u32 = 30;
+//     let height: u32 = 50;
+
+//     let dimensions: (u32, u32) = (width, height);
+//     let area_of_rectangle: u32 = area(dimensions);
+
+//     println!("The area of the rectangle is: {}", area_of_rectangle);
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32{
+//     dimensions.0 * dimensions.1
+// }
+
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// fn main() {
+
+//     let rectangle: Rectangle = Rectangle {
+//         width: 30, 
+//         height: 60,
+//     };
+
+//     let area_of_rectangle: u32 = area(&rectangle);
+
+//     println!("The area of the rectangle is: {}", area_of_rectangle);
+
+//     // printing the rectangle to the console 
+//     println!("rectangle is ::: {:?}", rectangle);
+
+//     // using the dbg macro to print the rectangle
+//     dbg!(rectangle);
+// }
+
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+
+// we can always use method instead of functions and here we define similar methods peculiar to the struct
+// methods are called in the context of a struct and their first parameter is always self
+
+impl Rectangle {
+
+    // this method below give the area of the rectangle 
+    // the sweet thing about this is that we use self to represenet the struct 
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    fn build(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+        }
+    }
+}
+
 
 fn main() {
 
-    // using the field init shorthand to create an instance of Rectangle
-    let rectangle: Rectangle = Rectangle(30, 50);
+    // let rectangle: Rectangle = Rectangle {
+    //     width: 30, 
+    //     height: 60,
+    // };
 
-    let area_of_rectangle: u32 = area(rectangle);
+    // let area_of_rectangle: u32 = rectangle.area();
 
-    println!("The area of the rectangle is: {}", area_of_rectangle);
+    // println!("The area of the rectangle is: {}", area_of_rectangle);
 
-}
+    // println!("rectangle is ::: {:?}", rectangle);
 
-fn area(rectangle: Rectangle) -> u32 {
-    rectangle.0 * rectangle.1
+    let rectangle_1: Rectangle = Rectangle {
+        width: 30, 
+        height: 60,
+    };
+
+    let rectangle_2: Rectangle = Rectangle {
+        width: 20, 
+        height: 40,
+    };
+
+    let rectangle_3: Rectangle = Rectangle {
+        width: 50, 
+        height: 70,
+    };
+
+    let can_rec_1_hold_rec_2: bool = rectangle_1.can_hold(&rectangle_2);
+    let can_rect_1_hold_rect_3: bool = rectangle_1.can_hold(&rectangle_3);
+
+
+    println!("Can rectangle 1 hold rectangle 2? {}", can_rec_1_hold_rec_2);
+
+    let built_rect: Rectangle = Rectangle::build(40, 80);
+    println!("Built rectangle is: {:?}", built_rect);
+
 }
