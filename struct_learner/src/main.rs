711310323133b1be1b426ef27ee105065643b780
blurbeast <leumasre@gmail.com>
2025-07-10 07:59:16 +0100

    still on struct

--- struct_learner/src/main.rs
+++ struct_learner/src/main.rs
@@ -24,19 +24,19 @@ struct User {
 
 // using the dot notation to access the fields of a struct
 // and also changing the value of a field
-fn main() {
+// fn main() {
 
-    let mut user: User = User {
-        username: String::from("blurbeast"),
-        active: true,
-        sign_in_count: 0,
-        email: String::from("email@blurbeast.co"),
-    };
+//     let mut user: User = User {
+//         username: String::from("blurbeast"),
+//         active: true,
+//         sign_in_count: 0,
+//         email: String::from("email@blurbeast.co"),
+//     };
 
-    let new_email: String = String::from("blurrybeast@blurbeast.co");
+//     let new_email: String = String::from("blurrybeast@blurbeast.co");
 
-    user.email = new_email;
-}
+//     user.email = new_email;
+// }
 
 // creating a function that builds the struxt
 // because user does not have to decide the active and sign_in_count fields, we use default values
@@ -62,4 +62,96 @@ fn build_user_struct(username: String, email: String) -> User {
     }
 
     // @Note: this is called field init shorthand
+}
+
+
+// fn main() {
+
+//     // using the build_user_struct function to create an instance of User
+//     let first_user:User = build_user_struct(String::from("blubreast"), String::from("blurbeast@blurbeast.co"));
+
+//     println!("First User: {}, {}, {}, {}", first_user.username, first_user.email, first_user.active, first_user.sign_in_count);
+
+//     // creating another user
+//     //using the instance of the first user to create another user
+//     let second_user: User = User{
+//         email: String::from("burry@blurbeast.co"),
+//         // we use the .. know as the spread operator to fill in other fields
+//         ..first_user
+//     }
+// }
+
+
+// fn main() {
+
+
+//     // we also have tuple structs
+//     struct Color(i32, i32, i32);
+//     struct Point(i32, i32, i32);
+
+//     let black = Color(0, 0, 0);
+//     let origin = Point(0, 0, 0);
+
+
+//     // when destructuring a tuple struct, we cdo it this way
+//     let Color(r, g, b) = black;
+//     println!("Color: {}, {}, {}", r, g, b);
+//     let Point(x, y, z) = origin;
+//     println!("Point: {}, {}, {}", x, y, z);
+// }
+
+
+// understanding why we used owned data types in the struct
+// struct Subject {
+//     name: String,
+//     subject_code: &str,
+//     credit_hours: u8,
+//     mentor: String,
+// }
+
+// fn main() {
+//     // we can use the owned data type in the struct
+//     // this is because the struct will own the data and it will be dropped when it goes out of scope
+//     let subject = Subject {
+//         name: String::from("Computer Science"),
+//         subject_code: "CS101",
+//         credit_hours: 3,
+//         mentor: String::from("Dr. Smith"),
+//     };
+
+//     println!("Subject: {}, {}, {}, {}", subject.name, subject.subject_code, subject.credit_hours, subject.mentor);
+// }
+
+// fn main() {
+//     // using an example like rectangle 
+//     let width: u32 = 30;
+//     let height: u32 = 50;
+
+//     let area_of_rectangle:u32 = area(width, height);
+
+//     println!("The area of the rectangle is: {}", area_of_rectangle);
+
+// }
+
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+
+struct Rectangle(u32, u32);
+
+fn main() {
+
+    // using the field init shorthand to create an instance of Rectangle
+    let rectangle: Rectangle = Rectangle(30, 50);
+
+    let area_of_rectangle: u32 = area(rectangle);
+
+    println!("The area of the rectangle is: {}", area_of_rectangle);
+
+}
+
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.0 * rectangle.1
 }
