94a936ab04393ca82ed890be807c883d3fae202e
blurbeast <leumasre@gmail.com>
2025-07-11 08:48:05 +0100

    learnt

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,63 @@
+
+
+
+// #[derive(Debug)]
+// enum IpAddrKind {
+//     v4,
+//     v6
+// }
+
+// #[derive(Debug)]
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// fn main() {
+//     let four: IpAddrKind = IpAddrKind::v4;
+//     let six: IpAddrKind = IpAddrKind::v6;
+
+//     let home: IpAddr = IpAddr {
+//         kind: four,
+//         address: String::from("127.0.0.1"),
+//     };
+
+//     let loopback: IpAddr = IpAddr {
+//         kind: six,
+//         address: String::from("::1"),
+//     };
+
+
+//     // println!("Home IP: {:?}", home);
+//     // println!("Loopback IP: {:?}", loopback);
+//     dbg!(home);
+//     // dbg!(loopback);
+// }
+
+
+// enum IpAddrKind {
+//     V4(String),
+//     V6(String),
+// }
+
+// fn main() {
+
+//     let home: IpAddrKind = IpAddrKind::V4(String::from("127.0.0:1"));
+//     let loopback: IpAddrKind = IpAddrKind::V6(String::from("::1"));
+// }
+
+
 fn main() {
-    println!("Hello, world!");
+    // dealing with option types 
+    let x: Option<u8> = Some(5);
+    let y: u8 = 4;
+
+    // in the above we have two variables x and y and while x is an Option<u8> type, y is a u8 type.
+    // hence we can say x and y are not of the same type 
+
+    // while x is a vairiant of the option type of u8 , y is a u8 type.
+
+    let sum: u8 = x.unwrap() + y;
+
+    println!("The sum is: {}", sum);
 }
---
+++ overflow/src/main.rs
@@ -0,0 +1,23 @@
+
+
+
+fn main() {
+
+    // understanding the overflow 
+
+//     let mut x: u8 = 255;
+//     let y: u8 = 1;
+
+//    x += y;
+
+//    println!("x: {}", x);
+
+   // array
+   let arr: [u8; 5] = [1, 2, 3, 4, 5];
+    let index: usize = 5;
+
+    // printing the value at an index
+    println!("Value at index {}: {}", 3, arr[3]);
+
+    println!("{:?}", arr);
+}
